153d051794404972b00fa55d81fa8531bbec91f0
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-05-01 00:12:56 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -139,7 +139,7 @@ if uploaded_file is not None:
         prediction = model_LR.predict(preface_transformed)
 
     st.subheader('💡 Predicted Difficulty Level')
-    st.success('<div class="pred-font"><p>' + str(prediction[0]) + '</p></div>', unsafe_allow_html=True)
+    st.write(f"<p class='pred-font'>{prediction[0]} </p>", unsafe_allow_html=True)
     
 # Save to profile functionality
 if st.button("Save to Profile"):
