bb5e51561305a311506b8d2f8ddd7a3792473e6d
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-04-30 13:59:25 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-url = 'https://github.com/RebeccaKessler/Machine_Learning/blob/main/streamlit/model_LR.pkl'
+url = 'https://github.com/RebeccaKessler/Machine_Learning/blob/main/streamlit/model_LR.pkl?raw=true'
 
 def load_model(url):
     response = requests.get(url)
