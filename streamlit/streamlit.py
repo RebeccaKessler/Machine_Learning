f38b0a5166aa83f40f2e79de13719ac3ac9476d2
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-04-30 12:06:09 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1um4F5Iq-68KsMnoNZtEDAHCiQZFjg1Wk
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
