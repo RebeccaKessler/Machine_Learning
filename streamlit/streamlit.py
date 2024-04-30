98b8b357b9470f107eeb9a1d18cee71258a08cb4
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-04-30 14:11:04 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
 import requests
-from charset_normalizer import from_bytes
+from docx import Document  # Import the library to handle .docx files
 
 url = 'https://github.com/RebeccaKessler/Machine_Learning/blob/main/streamlit/model_LR.pkl?raw=true'
 
@@ -20,25 +20,24 @@ st.title('Book Difficulty Prediction App')
 st.header('Upload the Preface of a Book')
 
 # File uploader allows user to add their own preface
-uploaded_file = st.file_uploader("Choose a file", type=["txt"])
+uploaded_file = st.file_uploader("Choose a file", type=["txt", "docx"])
 if uploaded_file is not None:
-    file_content = uploaded_file.read()
-    
-    # Attempt to detect the encoding
-    try:
-        results = from_bytes(file_content)  # Analyze the bytes to detect encoding
-        preface_text = results.best().text  # Get the best match and extract text
-        st.write("Uploaded Preface:")
-        st.write(preface_text)
+    if uploaded_file.type == "text/plain":
+        # Read as text file
+        preface_text = str(uploaded_file.read(), 'utf-8')
+    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        # Read as docx file
+        doc = Document(uploaded_file)
+        preface_text = "\n".join([para.text for para in doc.paragraphs])
 
-        # Predicting the difficulty
-        preface_transformed = vectorizer.transform([preface_text])
-        prediction = model_LR.predict(preface_transformed)
+    st.write("Uploaded Preface:")
+    st.write(preface_text)
 
-        st.subheader('Predicted Difficulty Level')
-        st.write(prediction[0])
-    except Exception as e:
-        st.error(f"Failed to decode the file: {str(e)}")
-        st.stop()
+    # Predicting the difficulty
+    preface_transformed = vectorizer.transform([preface_text])
+    prediction = model_LR.predict(preface_transformed)
+
+    st.subheader('Predicted Difficulty Level')
+    st.write(prediction[0])
 else:
-    st.warning('Please upload a text file.')
+    st.warning('Please upload a text or Word document.')
