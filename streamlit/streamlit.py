6b78f1ab76cf64a60c293d0fdf387e35790ff3d7
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-04-30 14:57:58 +0200

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -88,8 +88,8 @@ if uploaded_file is not None:
         preface_transformed = vectorizer.transform([preface_text])
         prediction = model_LR.predict(preface_transformed)
 
-    st.markdown('<div class="result-box">', unsafe_allow_html=True)
     st.subheader('💡 Predicted Difficulty Level')
+    st.markdown('<div class="result-box">', unsafe_allow_html=True)
     st.write(f"<p class='pred-font'>{prediction[0]}</p>", unsafe_allow_html=True)
     st.markdown('</div>', unsafe_allow_html=True)
 else:
