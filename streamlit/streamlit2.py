5ee6b2db916774ec7dabb8bb3336972673a4c3d0
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-05-01 20:14:29 +0200

    Update streamlit2.py

--- streamlit/streamlit2.py
+++ streamlit/streamlit2.py
@@ -118,7 +118,7 @@ with st.sidebar:
             st.session_state.filter_value = st.session_state.title_filter if filter_options == "Title" else st.session_state.pred_filter if filter_options == "Prediction Level" else None
             st.session_state['display_library'] = True
 
-if predict_button and uploaded_file is None or title is None:
+if predict_button and (uploaded_file is None or title is None):
     st.markdown("##")
     st.error("### ‼️ Please fill in title and upload file")
    
