101d2aa87c8192346c3ed0875edbc88fbb4f05f6
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-05-01 13:27:09 +0200

    Update streamlit2.py

--- streamlit/streamlit2.py
+++ streamlit/streamlit2.py
@@ -121,13 +121,14 @@ if predict_button and uploaded_file is not None and title:
     #Automatically save prediction
     save_to_library(title, prediction[0])
 
-filter_type = st.sidebar.radio("Filter by:", [ "Title", "Prediction Level"], index=0, key='filter_selection')
-if filter_type == "Title":
-    filter_value = st.sidebar.text_input("Enter Title:", key='filter_title_input')
-elif filter_type == "Prediction Level":
-    filter_value = st.sidebar.selectbox("Select Prediction Level", ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'], key='filter_prediction_select')
-
 if display_button:
     display_library("title" if filter_type == "Title" else "prediction" if filter_type == "Prediction Level" else None, filter_value)
     st.session_state['init'] = True
+    filter_type = st.sidebar.radio("Filter by:", [ "Title", "Prediction Level"], index=0, key='filter_selection')
+    if filter_type == "Title":
+        filter_value = st.sidebar.text_input("Enter Title:", key='filter_title_input')
+    elif filter_type == "Prediction Level":
+        filter_value = st.sidebar.selectbox("Select Prediction Level", ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'], key='filter_prediction_select')
+
+
 
