6ff27c0047a29d30606b735f34095e81d2672ca0
RebeccaKessler <154537658+RebeccaKessler@users.noreply.github.com>
2024-05-01 13:11:02 +0200

    Update streamlit2.py

--- streamlit/streamlit2.py
+++ streamlit/streamlit2.py
@@ -126,8 +126,8 @@ if display_button:
     st.session_state['init'] = True
 
 filter_type = st.sidebar.radio("Filter by:", [ "Title", "Prediction Level"], index=0, key='filter_selection')
-    if filter_type == "Title":
-        filter_value = st.sidebar.text_input("Enter Title:", key='filter_title_input')
-    elif filter_type == "Prediction Level":
-        filter_value = st.sidebar.selectbox("Select Prediction Level", ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'], key='filter_prediction_select')
+if filter_type == "Title":
+    filter_value = st.sidebar.text_input("Enter Title:", key='filter_title_input')
+elif filter_type == "Prediction Level":
+    filter_value = st.sidebar.selectbox("Select Prediction Level", ['A1', 'A2', 'B1', 'B2', 'C1', 'C2'], key='filter_prediction_select')
 
